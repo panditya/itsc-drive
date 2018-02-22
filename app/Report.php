287274f9ed178a240b56d44d9fb8c3fadb61a3c4
panditya <me@panditya.com>
2018-02-22 18:04:13 +0700

    fill the blank, added hasOne for type

--- app/Report.php
+++ app/Report.php
@@ -6,5 +6,45 @@ use Illuminate\Database\Eloquent\Model;
 
 class Report extends Model
 {
-    //
+    use Notifiable;
+    use SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'files_id', 'users_id', 'type_id', 'content', 'status'
+    ];
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = ['deleted_at'];
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = true;
+
+    /**
+     * Get all of the owning report models.
+     */
+    public function file()
+    {
+        return $this->belongsTo('App\File');
+    }
+
+    /**
+     * The report is must own one type.
+     */
+    public function type()
+    {
+        return $this->hasOne('App\ReportType');
+    }
 }
