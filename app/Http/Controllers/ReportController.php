98674fbf602478e27236ef58099b9b84976ae8d5
panditya <me@panditya.com>
2018-02-22 20:00:13 +0700

    fill the blank, added create & delete

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -2,9 +2,57 @@
 
 namespace App\Http\Controllers;
 
+use App\Report;
+
 use Illuminate\Http\Request;
+use Validator;
 
 class ReportController extends Controller
 {
-    //
+    /**
+     * Create a new report instance.
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+    public function store(Request $request){
+
+        // Validate the request...
+        $validator = Validator::make($request->all(), [
+            'files_id'        => 'required',
+            'user_id'         => 'required',
+            'type_id'         => 'required',
+            'content'         => 'required',
+            'status'          => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return redirect()->back()
+                        ->withErrors($validator)
+                        ->withInput();
+        }
+
+        $report = new Report;
+        $report->files_id = $request->files_id;
+        $report->user_id = $request->user_id;
+        $report->type_id = $request->type_id;
+        $report->content = $request->content;
+        $report->status = $request->status;
+        if ($report->save()) {
+          return redirect()->back();
+        }
+    }
+    /**
+    * Remove from database.
+    *
+    * @param  int  $id
+    * @return \Illuminate\Http\Response
+    */
+    public function destroy($id){
+      $report = Report::findOrFail($id);
+      // Delete from database
+      $report->delete();
+
+      return redirect()->back();
+    }
 }
