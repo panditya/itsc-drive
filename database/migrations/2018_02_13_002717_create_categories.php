cdde65271a86b0db3e9dd2bca12586fe2bbff776
panditya <me@panditya.com>
2018-02-22 18:01:15 +0700

    add dropIfExist

--- database/migrations/2018_02_13_002717_create_categories.php
+++ database/migrations/2018_02_13_002717_create_categories.php
@@ -13,6 +13,7 @@ class CreateCategories extends Migration
      */
     public function up()
     {
+        Schema::dropIfExists('categories');
         Schema::create('categories', function (Blueprint $table) {
           $table->increments('id');
           $table->string('name');
