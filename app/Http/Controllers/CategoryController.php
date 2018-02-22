fe0d018bef46d2b59cc77590e36e435da36aa30a
panditya <me@panditya.com>
2018-02-22 19:57:10 +0700

    little duty or might be cleaning

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -44,7 +44,6 @@ class CategoryController extends Controller
       */
       public function destroy($id){
         $category = Category::findOrFail($id);
-        // Delete from database
         $category->delete();
 
         return redirect()->back();
